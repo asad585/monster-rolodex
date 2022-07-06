3e303c4e7a81e085e96605fb2dc2286d2a9a0b92
Asad Ali <asad585@hotmail.com>
2022-07-06 13:43:04 +0500

    cards icons changed

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ const Card = (props) => {
         const {monster} = props;
         return (
             <div className='kity-posts'>
-                <img src={`https://robohash.org/${monster.id}?set=set4`} alt={`kitty ${monster.name}`} className='max-img' />
+                <img src={`https://robohash.org/${monster.id}?set=set2`} alt={`kitty ${monster.name}`} className='max-img' />
                 <h2 key={monster.id}>{monster.name}</h2>
                 <p className='kity-email'>{monster.email}</p>  
             </div>
